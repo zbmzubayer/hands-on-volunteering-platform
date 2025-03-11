bf82532124bb265eec2fea868582d8888e4b0a6a
ZBM Zubayer <zbmzubayer@gmail.com>
2025-03-12 03:55:47 +0600

    chore: add colors to base styles and tailwindCSS

--- packages/ui/src/tailwind-config/tailwind-base-config.ts
+++ packages/ui/src/tailwind-config/tailwind-base-config.ts
@@ -1,20 +1,33 @@
 import { type Config } from "tailwindcss";
 
 export const tailwindBaseConfig: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui/src/components/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
       fontFamily: {
         inter: ["var(--font-inter)"],
         geistMono: ["var(--font-geist-mono)"],
       },
       fontSize: {
         xxs: ["0.625rem", "0.875rem"],
+      },
+
+      colors: {
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
       },
     },
   },
